3cc8167a402760f74fce262b8898eaeadfd29af6
satoshi eric <stevexia1@gmail.com>
2018-10-28 20:42:12 -0400

    updated authentication

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello IST411! My name is Zifeng Xia");
-
+app.get("/",function(req,res){
+res.render("login.ejs");
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+app.post("/authenticate",function(req,res){
+    console.log(req.body);
+    var data = {
+        email:req.body.userEmail,
+        password: req.body.userPassword
+    }
+    if(data.email == 'testuser@email.com' && data.password == 'testpassword'){
+        res.render("home.ejs");
+    }
+    else{
+        res.render("failure.ejs");
+    }
+});
 
-console.log("Server running at http://localhost:%d", port);
+app.listen(8000, function(){
+    console.log("Serving applicaiton on port 8000");
+});
+
+
+// var server = http.createServer(function(request, response) {
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello IST411! My name is Zifeng Xia");
+
+// });
+
+
+
